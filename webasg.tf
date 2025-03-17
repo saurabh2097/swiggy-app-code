e597b4daf6f51f55e1dc71fb50de42a8fed008cd
saurabh2097 <saurabhchimankar97@gmail.com>
2025-03-17 10:27:44 +0530

    Update webasg.tf

--- webasg.tf
+++ webasg.tf
@@ -16,14 +16,14 @@ resource "aws_launch_template" "swiggy-web-template" {
   name_prefix   = "swiggy-web-template"
   image_id      = "ami-0ed194d7eff6d2f81"
   instance_type = "t2.micro"
-  key_name      = "devopsbyraham"
+  key_name      = "saurabhkeypair"
   network_interfaces {
     associate_public_ip_address = true
     security_groups             = [aws_security_group.swiggy-ec2-asg-sg.id]
   }
   user_data = base64encode(file("apache.sh"))
   lifecycle {
-    prevent_destroy = true
+    prevent_destroy = false
     ignore_changes  = all
   }
 }
